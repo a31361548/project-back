1a5cfdfc66d48596d74897ea13b7e3594c2002af
a31361548 <a31361548@gmail.com>
2024-02-23 11:44:15 +0800

    first commit

---
+++ models/articles.js
@@ -0,0 +1,34 @@
+import { Schema, model, ObjectId } from 'mongoose'
+// 等同於商品
+const articleSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, '缺少標題']
+  },
+  author: {
+    type: ObjectId,
+    required: [true, '缺少作者']
+  },
+  image: {
+    type: String,
+    required: [true, '缺少圖片']
+  },
+  content: {
+    type: String,
+    required: [true, '缺少內容']
+  },
+  type: {
+    type: String,
+    required: [true, '缺少類型'],
+    enum: {
+      values: ['經典調酒', '超商酒單'],
+      message: '酒單分類錯誤'
+    }
+  },
+  post: {
+    type: Boolean,
+    required: [true, '缺少發布狀態']
+  }
+}, { versionKey: false, timestamps: true })
+
+export default model('articles', articleSchema)
